d4c72b9f18cb4d6f3e47341d0ebfc8699ccb23e0
Exilus-g <134874951+Exilus-g@users.noreply.github.com>
2023-05-28 14:49:14 -0600

    Stationer class created

--- Evidence21/src/progama/Papeleria.java
+++ Evidence21/src/progama/Papeleria.java
@@ -1,5 +1,5 @@
 /**
- * Stationery class, contains all the attributes related to a product. 
+ * Stationer class, contains all the attributes related to a product. 
  * @author GabrielHL
  * @author AlejandroTC
  */
@@ -13,15 +13,64 @@ public class Papeleria {
 	private String categoria;
 	
 	public Papeleria(String nombre, String marca,String precio, int existencia, String categoria) {
-		this.nombre=nombre;
-		this.marca=marca;
-		this.precio=precio;
-		this.existencia=existencia;
-		this.categoria=categoria;
+		this.setNombre(nombre);
+		this.setMarca(marca);
+		this.setPrecio(precio);
+		this.setExistencia(existencia);
+		this.setCategoria(categoria);
 	}
+	/**
+     *  Get methods to obtain attribute values
+     * */
 	public String getNombre() {
 		return nombre;
 	}
+	
+	public String getMarca() {
+		return marca;
+	}
+	
+	public String getPrecio() {
+		return precio;
+	}
+	
+	public int getExistencia() {
+		return existencia;
+	}
+	
+	public String getCategoria() {
+		return categoria;
+	}
+
+	/**
+	 * Set methods for assigning values to attributes
+	 * @param nombre
+	 * @param marca
+	 * @param precio
+	 * @param existencia
+	 * @param categoria
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	
+	public void setPrecio(String precio) {
+		this.precio = precio;
+	}
+	
+	public void setExistencia(int existencia) {
+		this.existencia = existencia;
+	}
+	
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+
+	
 	
 	
 }
