146680655f9ee58e1885c5532c67f22a42fb276b
Exilus-g <134874951+Exilus-g@users.noreply.github.com>
2023-05-28 14:28:28 -0600

    Constructor aggregate

--- Evidence21/src/progama/Papeleria.java
+++ Evidence21/src/progama/Papeleria.java
@@ -1,17 +1,26 @@
 /**
- * Papeleria Class 
+ * Stationery class, contains all the attributes related to a product. 
+ * @author GabrielHL
+ * @author AlejandroTC
  */
 package progama;
 
 public class Papeleria {
 	private String nombre;
 	private String marca;
-	private 
+	private String precio;
 	private int existencia;
-	private String Categoria;
+	private String categoria;
 	
-	public Papeleria(String nombre, String marca, int existencia, String Categoria) {
-		
+	public Papeleria(String nombre, String marca,String precio, int existencia, String categoria) {
+		this.nombre=nombre;
+		this.marca=marca;
+		this.precio=precio;
+		this.existencia=existencia;
+		this.categoria=categoria;
+	}
+	public String getNombre() {
+		return nombre;
 	}
 	
 	
