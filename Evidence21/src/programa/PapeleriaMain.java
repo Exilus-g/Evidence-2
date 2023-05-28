b311b74a1be2be6ab70843d961091eff7ddc5509
alejandrootc <alejandrotc208@gmail.com>
2023-05-28 16:48:16 -0600

    Merge branch 'master' of https://github.com/Exilus-g/Evidence-2.git

--- Evidence21/src/programa/PapeleriaMain.java
+++ Evidence21/src/programa/PapeleriaMain.java
@@ -14,6 +14,37 @@ public class PapeleriaMain {
 		int existencia;
 		String categoria;
 		Papeleria p;
+		
+		
+		
+		
+		int opcion;
+        do {
+            System.out.println("=== Papelería ===");
+            System.out.println("1. Mostrar inventario ordenad.");
+            System.out.println("2. Agregar nuevo elemento.");
+            System.out.println("3. Salir");
+            System.out.print("Ingrese su opción: ");
+            opcion = sc.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    papeleria.mostrarInventario();
+                    break;
+                case 2:
+                    papeleria.agregarElemento();
+                    break;
+                case 3:
+                    System.out.println("¡Hasta luego!");
+                    break;
+                default:
+                    System.out.println("Opción inválida. Intente nuevamente.");
+                    break;
+            }
+        } while (opcion != 3);
+		
+		
+		
 		do {
 			System.out.println("Si desea finalizar escriba Fin");
 			System.out.println("nombre: ");
