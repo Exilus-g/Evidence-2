d24523484e7c36ddce9bfb043a8cb778466c0a7f
Exilus-g <134874951+Exilus-g@users.noreply.github.com>
2023-05-28 14:19:17 -0600

    P

--- Evidence21/src/progama/Papeleria.java
+++ Evidence21/src/progama/Papeleria.java
@@ -1,11 +1,18 @@
+/**
+ * Papeleria Class 
+ */
 package progama;
 
 public class Papeleria {
 	private String nombre;
 	private String marca;
+	private 
 	private int existencia;
 	private String Categoria;
 	
+	public Papeleria(String nombre, String marca, int existencia, String Categoria) {
+		
+	}
 	
 	
 }
