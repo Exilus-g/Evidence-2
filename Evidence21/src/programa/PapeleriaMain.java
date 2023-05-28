73ed86aa6609d61673dbb574a439d0868bf37cdd
Exilus-g <134874951+Exilus-g@users.noreply.github.com>
2023-05-28 16:54:46 -0600

    c

--- Evidence21/src/programa/PapeleriaMain.java
+++ Evidence21/src/programa/PapeleriaMain.java
@@ -7,6 +7,7 @@ import java.io.*;
 
 public class PapeleriaMain {
 	public static Vector<Papeleria>producto= new Vector<>();
+	
 	public static void escribeVector(Scanner sc){
 		String nombre;
 		String marca;
@@ -14,37 +15,6 @@ public class PapeleriaMain {
 		int existencia;
 		String categoria;
 		Papeleria p;
-		
-		
-		
-		
-		int opcion;
-        do {
-            System.out.println("=== Papelería ===");
-            System.out.println("1. Mostrar inventario ordenad.");
-            System.out.println("2. Agregar nuevo elemento.");
-            System.out.println("3. Salir");
-            System.out.print("Ingrese su opción: ");
-            opcion = sc.nextInt();
-
-            switch (opcion) {
-                case 1:
-                    papeleria.mostrarInventario();
-                    break;
-                case 2:
-                    papeleria.agregarElemento();
-                    break;
-                case 3:
-                    System.out.println("¡Hasta luego!");
-                    break;
-                default:
-                    System.out.println("Opción inválida. Intente nuevamente.");
-                    break;
-            }
-        } while (opcion != 3);
-		
-		
-		
 		do {
 			System.out.println("Si desea finalizar escriba Fin");
 			System.out.println("nombre: ");
@@ -82,6 +52,32 @@ public class PapeleriaMain {
 		escribeVector(sc);
 		escribeCsv();
 		sc.close();
+		int opcion;
+        do {
+            System.out.println("=== Papelería ===");
+            System.out.println("1. Mostrar inventario ordenad.");
+            System.out.println("2. Agregar nuevo elemento.");
+            System.out.println("3. Salir");
+            System.out.print("Ingrese su opción: ");
+            opcion = sc.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    papeleria.mostrarInventario();
+                    break;
+                case 2:
+                    papeleria.agregarElemento();
+                    break;
+                case 3:
+                    System.out.println("¡Hasta luego!");
+                    break;
+                default:
+                    System.out.println("Opción inválida. Intente nuevamente.");
+                    break;
+            }
+        } while (opcion != 3);
+		
+		
 	}
 	
 }
