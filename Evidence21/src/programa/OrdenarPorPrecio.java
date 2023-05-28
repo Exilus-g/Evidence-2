d0502b211cedc90b0b9a46ccb7655a37ed155973
alejandrootc <alejandrotc208@gmail.com>
2023-05-28 16:34:54 -0600

    Comentarios

--- Evidence21/src/programa/OrdenarPorPrecio.java
+++ Evidence21/src/programa/OrdenarPorPrecio.java
@@ -2,10 +2,20 @@ package programa;
 
 import java.util.*;
 
-class OrdenarPorPrecio implements Comparator<Papeleria> 
-{
-   public int compare(Papeleria p1, Papeleria p2)
-    {
+/**
+ * This class implements the Comparator interface to define a custom sorting order for Papeleria objects based on their price.
+ */
+class OrdenarPorPrecio implements Comparator<Papeleria> {
+
+    /**
+     * Compares two Papeleria objects based on their price.
+     *
+     * @param p1 the first Papeleria object to compare
+     * @param p2 the second Papeleria object to compare
+     * @return a negative integer if p1's price is less than p2's price,
+     *         zero if they are equal, or a positive integer if p1's price is greater than p2's price
+     */
+    public int compare(Papeleria p1, Papeleria p2) {
         return p1.getPrecio().compareTo(p1.getPrecio());
     }
 }
